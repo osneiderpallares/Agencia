8a5ed3364056a70c6079c9827692fce31778852d
osneiderpallares <osneider.pallares20@gmail.com>
2023-12-04 10:43:28 -0500

    Agregar archivos de proyecto.

---
+++ Agencia/Controllers/UsuariosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Agencia.Models;
+
+namespace Agencia.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private AGENCIAModels db = new AGENCIAModels();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            var usuarios = db.Usuarios.Include(u => u.tiposDocumentos);
+            return View(usuarios.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.tipoDocumento = new SelectList(db.tiposDocumentos, "id", "nombre");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,tipoDocumento,numeroDocumento,nombres,apellidos,email,genero,telefono,fechaNacimiento,nombreUsuario,clave")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.tipoDocumento = new SelectList(db.tiposDocumentos, "id", "nombre", usuarios.tipoDocumento);
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.tipoDocumento = new SelectList(db.tiposDocumentos, "id", "nombre", usuarios.tipoDocumento);
+            return View(usuarios);
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,tipoDocumento,numeroDocumento,nombres,apellidos,email,genero,telefono,fechaNacimiento,nombreUsuario,clave")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.tipoDocumento = new SelectList(db.tiposDocumentos, "id", "nombre", usuarios.tipoDocumento);
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuarios usuarios = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Agencia/Controllers/tiposDocumentosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Agencia.Models;
+
+namespace Agencia.Controllers
+{
+    public class tiposDocumentosController : Controller
+    {
+        private AGENCIAModels db = new AGENCIAModels();
+
+        // GET: tiposDocumentos
+        public ActionResult Index()
+        {
+            return View(db.tiposDocumentos.ToList());
+        }
+
+        // GET: tiposDocumentos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tiposDocumentos tiposDocumentos = db.tiposDocumentos.Find(id);
+            if (tiposDocumentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tiposDocumentos);
+        }
+
+        // GET: tiposDocumentos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tiposDocumentos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre")] tiposDocumentos tiposDocumentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tiposDocumentos.Add(tiposDocumentos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tiposDocumentos);
+        }
+
+        // GET: tiposDocumentos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tiposDocumentos tiposDocumentos = db.tiposDocumentos.Find(id);
+            if (tiposDocumentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tiposDocumentos);
+        }
+
+        // POST: tiposDocumentos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre")] tiposDocumentos tiposDocumentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tiposDocumentos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tiposDocumentos);
+        }
+
+        // GET: tiposDocumentos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tiposDocumentos tiposDocumentos = db.tiposDocumentos.Find(id);
+            if (tiposDocumentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tiposDocumentos);
+        }
+
+        // POST: tiposDocumentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tiposDocumentos tiposDocumentos = db.tiposDocumentos.Find(id);
+            db.tiposDocumentos.Remove(tiposDocumentos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
